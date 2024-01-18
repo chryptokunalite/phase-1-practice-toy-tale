ec20a9056e4f13a6264e055ac0ecebae191209d9
Allan Saenz <allan.d.saenz@gmail.com>
2024-01-17 23:00:23 -0500

    done

--- src/index.js
+++ src/index.js
@@ -13,3 +13,88 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+document.addEventListener('DOMContentLoaded', function(){
+//MY CODE
+const toyForm = document.querySelector('.add-toy-form');
+
+//Build Toy Cards
+function renderToy(toy){
+let toyBox = document.getElementById('toy-collection');
+    let toyCard = document.createElement('div')
+    toyCard.className = 'card';
+    const cardName = toy.toyName ? toy.toyName : toy.name; 
+    toyCard.innerHTML = `
+    <h2>${cardName}</h2>
+    <img src="${toy.image}" class="toy-avatar"/>
+    <p class="likes">${toy.likes}</p>
+    <button class="like-btn" id="[${toy.id}]">Like ❤️</button>
+    `
+    toyCard.querySelector('.like-btn').addEventListener('click', function likeToy(){
+      toy.likes+= 1
+      toyCard.querySelector('.likes').textContent = toy.likes
+      updateLikes(toy)
+    })
+//Attach Toy Card to DOM
+toyBox.append(toyCard)
+}
+//MY CODE ^
+
+// Fetch GET Request for existing toys
+// document.addEventListener('DOMContentLoaded', function(){
+
+  fetch('http://localhost:3000/toys')
+    .then(function(response){
+    return response.json(); 
+})
+    .then(function(data){
+    data.forEach(toy => renderToy(toy))
+})
+
+//Fetch POST Request for new toys & prevent default form behavior
+toyForm.addEventListener('submit', function(event){
+  event.preventDefault();
+
+const toyName = document.querySelector('[name="name"]').value; 
+const image = document.querySelector('[name="image"]').value; 
+const likes = 0; 
+
+  let newToy = {
+    toyName: toyName, 
+    image: image, 
+    likes: likes
+  };
+
+fetch('http://localhost:3000/toys',{
+  method: 'POST', 
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(newToy)
+})
+.then(function (response){
+  return response.json();
+}) 
+.then(function(data){
+ return renderToy(data) 
+})
+})
+
+//Fetch PATCH Request for likes of all toys
+function updateLikes(toy){
+  fetch(`http://localhost:3000/toys/${toy.id}`,{
+    method:'PATCH', 
+    headers:{
+      'Content-Type': 'application/json'
+    }, 
+    body: JSON.stringify(toy)
+  })
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(toy){
+    console.log(toy)
+    return toy
+  })
+}
+
+})
